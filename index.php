d25839bea3014da9448443ccc5ff4ab20a3e81a3
Mathias Kulhandjian <mkulhand@z1r14p5.le-101.fr>
2018-06-14 16:33:39 +0200

    dawd

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="content">
+            <div class="categ-box">
+                <div class="categ-cover"></div>
+                <img src="budo.jpg">
+                <div class="categ-content">
+                    <h2>BUDO</h2>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
